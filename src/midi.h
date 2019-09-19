0226c2aff136ab1e7989b4f2ccdfaa693f54bb37
Todd Cope <xander4jesus@yahoo.com>
2019-09-19 18:16:06 -0500

    Export MIDI instrument name array.

--- src/midi.h
+++ src/midi.h
@@ -3,6 +3,8 @@
 
 #include "MIDIA5/midia5.h"
 
+extern const char * ii_midi_instrument_name[128];
+
 void ii_send_program_change(MIDIA5_OUTPUT_HANDLE * hp, int channel, int program);
 void ii_send_note_on(MIDIA5_OUTPUT_HANDLE * hp, int channel, int note, int velocity);
 void ii_send_note_off(MIDIA5_OUTPUT_HANDLE * hp, int channel, int note, int velocity);
